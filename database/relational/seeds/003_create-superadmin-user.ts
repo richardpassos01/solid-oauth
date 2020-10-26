24da98e6a52ef8fb5e0e3e6d3d263918f1b52979
richardpassos01 <richard.passos@hotmail.com>
2020-10-26 00:14:48 -0300

    refactor: seed

--- database/relational/seeds/003_create-superadmin-user.ts
+++ database/relational/seeds/003_create-superadmin-user.ts
@@ -11,7 +11,7 @@ export async function seed(knex: Knex): Promise<void> {
       username: 'admin001',
       document: 92811841091,
       salt: '6d8e72d5fd2c4e914e746d104fadf914',
-      hash: 'd091c78f8c364c2b84b19014c6c08fde708e800953689b6ef217a0de9ef2baee6952cbd4d05213014af9e4f24fc8fe80fbd88f1f7fcb2475aaf791805edb8e23'
+      hash: 'd091c78f8c364c2b84b19014c6c08fde708e800953689b6ef217a0de9ef2baee6952cbd4d05213014af9e4f24fc8fe80fbd88f1f7fcb2475aaf791805edb8e23',
     },
   ]);
 }
